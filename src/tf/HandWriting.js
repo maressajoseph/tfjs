ccf20ee9bd24b243483110d7e5cc89592f1a71f2
Maressa Joseph <maressa.joseph@jungleminds.nl>
2018-10-05 10:43:36 +0200

    Add tf model and tf training for linear regression learning

---
+++ src/tf/HandWriting.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { tf } from './index'
+
+class HandWriting extends Component {
+  componentDidMount() {
+    
+  }
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+export default HandWriting
---
+++ src/tf/LinearRegression.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { modelLinear as model, tf } from './index'
+
+class LinearRegression extends Component {
+  state = {
+    training: true,
+    result: ''
+  }
+
+  componentDidMount() {
+    // training data
+    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1])
+    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1])
+
+    // train model w/training data (epochs property specifies how many times tf is going thru training set)
+    // result of the fit method is a promise => callback fn when training is done
+    model.fit(xs, ys, { epochs: 500 }).then(() => {
+      model.predict(tf.tensor2d([5], [1,1])).print()
+      this.setState({
+        training: false
+      })
+    })
+  }
+
+  handleClick = () => {
+    const val = document.getElementById('inputValue').value
+    this.setState({
+      result: String(model.predict(tf.tensor2d([val], [1,1])))
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>
+          Linear Regression
+        </h2>
+        <label>Input value:</label>
+        <input type="text" id="inputValue" disabled={this.state.training}/><br />
+        <button onClick={this.handleClick}>{this.state.training ? 'Model is training..' : 'Predict'}</button>
+        <p>Result: {this.state.result}</p>
+      </div>
+    )
+  }
+}
+
+export default LinearRegression
